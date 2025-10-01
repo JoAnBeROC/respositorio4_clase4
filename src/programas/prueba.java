46648d396c5ebefb8fe8b4ecc3286bcf55c6f047
USER <USER@DESKTOP-LQF55N2>
2025-10-01 08:01:04 -0500

    carga inicial

--- src/programas/prueba.java
+++ src/programas/prueba.java
@@ -10,5 +10,6 @@ public class prueba {
         System.out.println("Mi nombre es: josue");
         System.out.println("Mi edad es: 25 años");
         System.out.println("Mi genero es: masculino");
+        System.out.println("Mi correo es: josue.berrocal@unmsm.edu.pe");
     }
 }
